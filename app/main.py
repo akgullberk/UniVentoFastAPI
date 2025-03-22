30540400acffeab840f7227907242297474d47ee
Berk Akgül <berkak_gul51@hotmail.com>
2025-03-22 21:00:47 +0300

    requirements.txt eklendi

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.clubs import router as clubs_router
 
 app = FastAPI()
+
+# CORS ayarlarını yapılandırıyoruz
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # İzin verilen kaynaklar
+    allow_credentials=True,
+    allow_methods=["*"],  # Tüm HTTP yöntemlerine izin ver
+    allow_headers=["*"],  # Tüm başlıklara izin ver
+)
+
 app.include_router(clubs_router)
 
