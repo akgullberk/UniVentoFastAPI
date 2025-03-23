187f5a0b2255a247baa73be6ad641af91552eeb0
Berk Akgül <berkak_gul51@hotmail.com>
2025-03-23 20:44:16 +0300

    cors denemesi

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # CORS ayarlarını yapılandırıyoruz
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # İzin verilen kaynaklar
+    allow_origins=["http://16.170.205.160"],  # Sunucu adresi
     allow_credentials=True,
     allow_methods=["*"],  # Tüm HTTP yöntemlerine izin ver
     allow_headers=["*"],  # Tüm başlıklara izin ver
